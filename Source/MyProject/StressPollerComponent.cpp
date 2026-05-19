8d2723be3ed398e782b800fce33717f511d4bd29
Giada Giacobbe <ggiacobbe13@gmail.com>
2026-05-19 15:31:54 -0400

    built StressPollerComponent

--- Source/MyProject/StressPollerComponent.cpp
+++ Source/MyProject/StressPollerComponent.cpp
@@ -1,27 +1,82 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "StressPollerComponent.h"
+#include "TimerManager.h"
+#include "Json.h"
+#include "JsonUtilities.h"
 
-// Sets default values
-AStressPollerComponent::AStressPollerComponent()
+UStressPollerComponent::UStressPollerComponent()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
-
+    PrimaryComponentTick.bCanEverTick = false;
+    PollInterval = 1.0f;
 }
 
-// Called when the game starts or when spawned
-void AStressPollerComponent::BeginPlay()
+void UStressPollerComponent::BeginPlay()
 {
-	Super::BeginPlay();
-	
+    Super::BeginPlay();
 }
 
-// Called every frame
-void AStressPollerComponent::Tick(float DeltaTime)
+void UStressPollerComponent::StartPolling(const FString& URL, float IntervalSeconds)
 {
-	Super::Tick(DeltaTime);
+    EndpointURL = URL;
+    PollInterval = IntervalSeconds;
 
+    GetWorld()->GetTimerManager().SetTimer(
+        PollTimer,
+        this,
+        &UStressPollerComponent::PollServer,
+        PollInterval,
+        true
+    );
 }
 
+void UStressPollerComponent::StopPolling()
+{
+    GetWorld()->GetTimerManager().ClearTimer(PollTimer);
+}
+
+void UStressPollerComponent::PollServer()
+{
+    TSharedRef<IHttpRequest, ESPMode::ThreadSafe> Request =
+        FHttpModule::Get().CreateRequest();
+
+    Request->SetURL(EndpointURL);
+    Request->SetVerb("GET");
+
+    Request->OnProcessRequestComplete().BindUObject(
+        this,
+        &UStressPollerComponent::OnResponseReceived
+    );
+
+    Request->ProcessRequest();
+}
+
+void UStressPollerComponent::OnResponseReceived(
+    FHttpRequestPtr Request,
+    FHttpResponsePtr Response,
+    bool bWasSuccessful
+)
+{
+    float StressValue = 0.0f;
+
+    if (bWasSuccessful && Response.IsValid())
+    {
+        FString Content = Response->GetContentAsString();
+
+        TSharedPtr<FJsonObject> JsonObject;
+        TSharedRef<TJsonReader<>> Reader = TJsonReaderFactory<>::Create(Content);
+
+        if (FJsonSerializer::Deserialize(Reader, JsonObject) && JsonObject.IsValid())
+        {
+            StressValue = JsonObject->GetNumberField("stress");
+        }
+        else
+        {
+            bWasSuccessful = false;
+        }
+    }
+    else
+    {
+        bWasSuccessful = false;
+    }
+
+    OnStressReceived.Broadcast(StressValue, bWasSuccessful);
+}
