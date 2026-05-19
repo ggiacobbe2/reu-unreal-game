bda6db221ac7cb2bc471415d14fa1d97a8334f7d
Giada Giacobbe <ggiacobbe13@gmail.com>
2026-05-19 15:29:31 -0400

    built StressPollerComponent

--- Source/MyProject/StressPollerComponent.h
+++ Source/MyProject/StressPollerComponent.h
@@ -1,26 +1,42 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
-#include "GameFramework/Actor.h"
+#include "Components/ActorComponent.h"
+#include "Http.h"
 #include "StressPollerComponent.generated.h"
 
-UCLASS()
-class MYPROJECT_API AStressPollerComponent : public AActor
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(
+    FOnStressReceived,
+    float, StressValue,
+    bool, bSuccess
+);
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class MYPROJECT_API UStressPollerComponent : public UActorComponent
 {
-	GENERATED_BODY()
-	
-public:	
-	// Sets default values for this actor's properties
-	AStressPollerComponent();
+    GENERATED_BODY()
+
+public:
+    UStressPollerComponent();
+
+    UFUNCTION(BlueprintCallable, Category="Stress")
+    void StartPolling(const FString& URL, float IntervalSeconds);
+
+    UFUNCTION(BlueprintCallable, Category="Stress")
+    void StopPolling();
+
+    UPROPERTY(BlueprintAssignable, Category="Stress")
+    FOnStressReceived OnStressReceived;
 
 protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
+    virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+private:
+    void PollServer();
+    void OnResponseReceived(FHttpRequestPtr Request, FHttpResponsePtr Response, bool bWasSuccessful);
 
+    FTimerHandle PollTimer;
+
+    FString EndpointURL;
+    float PollInterval;
 };
